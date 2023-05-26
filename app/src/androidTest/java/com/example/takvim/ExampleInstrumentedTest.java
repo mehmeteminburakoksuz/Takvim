a70a0558d4d860b5ec907f4123609b7575ca1ea8
mehmeteminburakoksuz <93429491+mehmeteminburakoksuz@users.noreply.github.com>
2023-05-26 15:17:10 +0300

    Add files via upload

--- app/src/androidTest/java/com/example/takvim/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/takvim/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.takvim", appContext.getPackageName());
+        assertEquals("codewithcal.au.calendarappexample", appContext.getPackageName());
     }
 }
