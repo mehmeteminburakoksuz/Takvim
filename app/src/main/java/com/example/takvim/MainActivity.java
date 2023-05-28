112cb86766cae6c263b6e4ef9a73cab3b9e7d0be
SabitCanTurunc <turuncsc@gmail.com>
2023-05-29 01:43:48 +0300

    LoginActivity added

--- app/src/main/java/com/example/takvim/MainActivity.java
+++ app/src/main/java/com/example/takvim/MainActivity.java
@@ -10,8 +10,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,26 +23,11 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
-        EditText username = findViewById(R.id.username);
-        EditText password = findViewById(R.id.password);
-        Button loginButton = findViewById(R.id.loginButton);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
-                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                    setContentView(R.layout.activity_main);
-                    initWidgets();
-                    CalendarUtils.selectedDate = LocalDate.now();
-                    setMonthView();
-                } else {
-                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        initWidgets();
+        CalendarUtils.selectedDate = LocalDate.now();
+        setMonthView();
     }
 
     private void initWidgets() {
